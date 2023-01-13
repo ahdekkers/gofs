e8304e8590f669defd32e82790bc2917428686c2
ahdekkers <alex@flowfabricator.com>
2023-01-13 11:45:26 +0000

    Revert "now only serves binaries"
    This reverts commit fe3b1852df1b4fa0659e242b53e9d4f4e25d7584.

--- gofs/gofs_test.go
+++ gofs/gofs_test.go
@@ -1,6 +1,8 @@
 package gofs
 
 import (
+	"fmt"
+	"github.com/phayes/freeport"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
@@ -10,8 +12,7 @@ var addr string
 
 func TestCreate_NoRootDir(t *testing.T) {
 	opts := Opts{}
-	fs, err := Create(opts)
-	assert.Nil(t, fs)
+	err := Create(opts)
 	assert.NotNil(t, err)
 	if err != nil {
 		assert.Contains(t, err.Error(), "failed to read root dir")
@@ -22,10 +23,28 @@ func TestCreate_InvalidAddr(t *testing.T) {
 	opts := Opts{
 		RootDir: os.TempDir(),
 	}
-	fs, err := Create(opts)
-	assert.Nil(t, fs)
+	err := Create(opts)
 	assert.NotNil(t, err)
 	if err != nil {
 		assert.Contains(t, err.Error(), "port not specified")
 	}
+}
+
+func setupTestServer(t *testing.T) {
+	port, err := freeport.GetFreePort()
+	if err != nil {
+		t.Fatalf("Failed to get free port: %v", err)
+	}
+
+	opts := Opts{
+		Addr:     "127.0.0.1",
+		Port:     port,
+		LogLevel: "TRACE",
+		RootDir:  os.TempDir(),
+	}
+	err = Create(opts)
+	if err != nil {
+		t.Fatalf("Failed to create server: %v", err)
+	}
+	addr = fmt.Sprintf("127.0.0.1:%d", port)
 }
